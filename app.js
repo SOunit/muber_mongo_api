0fc47654c8e0f1534554f306d5737ae7b001bd44
SOunit <workbookwork7@gmail.com>
2020-12-13 20:57:07 +0900

    added api request test

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const app = express();
 
+// http://localhost:3050/api
+app.get('/api', (req, res, next) => {
+  console.log('/api');
+  res.send({ hi: 'there' });
+});
+
+// http://localhost:3050/
+app.get('/', (req, res, next) => {
+  console.log('/');
+  res.send({ hi: 'there' });
+});
+
 module.exports = app;
