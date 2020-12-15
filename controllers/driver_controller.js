067107aa110729ff6d9d5cfed5a9101387c0aa1c
SOunit <workbookwork7@gmail.com>
2020-12-15 21:20:09 +0900

    added delete test

--- controllers/driver_controller.js
+++ controllers/driver_controller.js
@@ -4,12 +4,36 @@ module.exports = {
   greeting(req, res) {
     res.send({ hi: 'there' });
   },
+
   create(req, res, next) {
     const driverProps = req.body;
 
     Driver.create(driverProps)
       .then((driver) => {
         return res.send(driver);
+      })
+      .catch(next);
+  },
+
+  edit(req, res, next) {
+    const driverId = req.params.id;
+    const driverProps = req.body;
+
+    Driver.findByIdAndUpdate({ _id: driverId }, driverProps).then(() => {
+      return Driver.findById({ _id: driverId })
+        .then((driver) => {
+          return res.send(driver);
+        })
+        .catch(next);
+    });
+  },
+
+  delete(req, res, next) {
+    const driverId = req.params.id;
+
+    Driver.findByIdAndRemove({ _id: driverId })
+      .then((driver) => {
+        res.status(204).send(driver);
       })
       .catch(next);
   },
--- routes/routes.js
+++ routes/routes.js
@@ -3,11 +3,11 @@ const driverController = require('../controllers/driver_controller');
 module.exports = (app) => {
   // http://localhost:3050/api
   app.get('/api', driverController.greeting);
-
   // http://localhost:3050/
   // app.get('/', (req, res, next) => {
   //   res.send({ hi: 'there' });
   // });
-
   app.post('/api/drivers', driverController.create);
+  app.put('/api/drivers/:id', driverController.edit);
+  app.delete('/api/drivers/:id', driverController.delete);
 };
